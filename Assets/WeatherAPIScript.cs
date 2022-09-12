964d624b05c47b9e098584bf17e5cdf23ef33dd3
tameem22 <tameemusa@gmail.com>
2022-09-12 15:21:21 -0500

    first commit

---
+++ Assets/WeatherAPIScript.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Networking;
+
+
+public class WeatherAPIScript : MonoBehaviour
+{
+    public GameObject weatherTextObject;
+       string url = "http://api.openweathermap.org/data/2.5/weather?lat=41.88&lon=-87.6&APPID=9eaf6802db8c9921ca1cc5b6315fb3ef&units=imperial";
+    
+   
+    void Start()
+    {
+
+    // wait a couple seconds to start and then refresh every 900 seconds
+
+       InvokeRepeating("GetDataFromWeb", 2f, 900f);
+   }
+
+   void GetDataFromWeb()
+   {
+
+       StartCoroutine(GetRequest(url));
+   }
+
+    IEnumerator GetRequest(string uri)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+
+            if (webRequest.isNetworkError)
+            {
+                Debug.Log(": Error: " + webRequest.error);
+            }
+            else
+            {
+                // print out the weather data to make sure it makes sense
+                Debug.Log(":\nReceived: " + webRequest.downloadHandler.text);
+                int startTemp = webRequest.downloadHandler.text.IndexOf("temp",0);
+            	int endTemp = webRequest.downloadHandler.text.IndexOf(",",startTemp);
+				double tempF = float.Parse(webRequest.downloadHandler.text.Substring(startTemp+6, (endTemp-startTemp-6)));
+				int easyTempF = Mathf.RoundToInt((float)tempF);
+                //Debug.Log ("integer temperature is " + easyTempF.ToString());
+                int startConditions = webRequest.downloadHandler.text.IndexOf("main",0);
+                int endConditions = webRequest.downloadHandler.text.IndexOf(",",startConditions);
+                string conditions = webRequest.downloadHandler.text.Substring(startConditions+7, (endConditions-startConditions-8));
+                //Debug.Log(conditions);
+
+                weatherTextObject.GetComponent<TextMeshPro>().text = "" + easyTempF.ToString() + "°F\n" + conditions;
+            }
+        }
+    }
+
+}
