0405a716f177ff99423c6d8dfe69429b2fa0b903
tameem22 <tameemusa@gmail.com>
2022-09-17 13:15:24 -0500

    added weather and time to classCube

---
+++ Assets/timeTeller2.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class timeTeller2 : MonoBehaviour
+{
+    public GameObject timeTextObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    InvokeRepeating("UpdateTime", 0f, 10f);   
+    }
+
+   
+    void UpdateTime()
+    {
+    timeTextObject.GetComponent<TextMeshPro>().text = System.DateTime.Now.ToString("HH:mm tt");
+       
+    }
+}
